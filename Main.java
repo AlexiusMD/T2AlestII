acc5563d8be9e0c2cf19a865ad3a7398f658a3c5
Felipe Valiati <felipe.cvaliati@gmail.com>
2024-06-23 22:26:41 -0300

    eliminacao de casos inviaveis e menu

--- Main.java
+++ Main.java
@@ -2,7 +2,31 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Box> boxes = ListReader.readFile("./casosT10/teste500.txt");
+        HashMap<Integer, String> casos = new HashMap<>();
+
+        casos.put(1, "teste10");
+        casos.put(2, "teste20");
+        casos.put(3, "teste50");
+        casos.put(4, "teste100");
+        casos.put(5, "teste200");
+        casos.put(6, "teste300");
+        casos.put(7, "teste500");
+        casos.put(8, "teste1000");
+        casos.put(9, "teste2000");
+
+        System.out.println("Digite o caso de teste desejado (recomendo um pc quantico caso queira rodar o caso 7 pra cima (só deus sabe o resultado))");
+
+        for (Integer i : casos.keySet()) {
+            System.out.println(i + ": " + casos.get(i));
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        Integer caso = input.nextInt();
+
+        input.close();
+
+        ArrayList<Box> boxes = ListReader.readFile("./casosT10/" + casos.get(caso) + ".txt");
 
         Graph<Box> G = new Graph<>();
 
@@ -15,22 +39,35 @@ public class Main {
 
         LinkedList<Box> longest = new LinkedList<>();
 
-        for (Box v : G.getVerts()) {
-            System.out.println("Getting longest path for box " + v);
+        float count = 1;
 
-            LongestPathSearch<Box> lps = new LongestPathSearch<>(G, v);
+        Set<Box> verts = G.getVerts();
+        int size = verts.size();
+        for (Box v : verts) {
+            if (G.getAdjRev(v) == null) { //se n tiver caixa acima (elegivel a ser o caminho mais longo)
+                System.out.println("Loading " + (100 * count/size) + "%");
 
-            LinkedList<Box> path = lps.longestPath();
-            
-            if (path.size() > longest.size()) {
-                longest = path;
+                LongestPathSearch<Box> lps = new LongestPathSearch<>(G, v);
+
+                LinkedList<Box> path = lps.longestPath();
+                
+                if (path.size() > longest.size()) {
+                    longest = path;
+                }
             }
+            
+            count++;
         }
+
+        count = 0;
 
         System.out.println("Caminho mais longo:");
 
         for (Box v : longest) {
             System.out.println(v);
+            count++;
         }
+
+        System.out.println("O caminho mais longo é composto por " + count + " caixas");
     }
 }
